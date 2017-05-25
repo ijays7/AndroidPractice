30527679542fc973de6970a23e2ac26a403b7a8e
songjie <songjie@yunyao-inc.com>
2017-05-25 21:40:59 +0800

    Add main page UI

--- app/src/main/java/com/ijays/apolo/MainActivity.java
+++ app/src/main/java/com/ijays/apolo/MainActivity.java
@@ -3,20 +3,24 @@ package com.ijays.apolo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tabLayout)
+    TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
