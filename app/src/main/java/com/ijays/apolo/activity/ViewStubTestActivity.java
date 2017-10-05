bfd7dddf21f319dd81cd0780e4b45d7f7c3d7f2c
songjie <ijaysdev@gmail.com>
2017-10-05 21:45:13 +0800

    Update gradle version

--- app/src/main/java/com/ijays/apolo/activity/ViewStubTestActivity.java
+++ app/src/main/java/com/ijays/apolo/activity/ViewStubTestActivity.java
@@ -1,8 +1,17 @@
 package com.ijays.apolo.activity;
 
+import android.animation.AnimatorSet;
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
+import android.view.View;
 import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.ijays.apolo.R;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -14,7 +23,8 @@ import butterknife.OnClick;
 public class ViewStubTestActivity extends BaseActivity {
     @BindView(R.id.view_stub)
     ViewStub mViewStub;
-
+    @BindView(R.id.iv_spring_anim)
+    ImageView mIvSpringAnim;
 
     @Override
     protected int getLayoutId() {
@@ -28,5 +38,24 @@ public class ViewStubTestActivity extends BaseActivity {
         if (mViewStub.getParent() != null)
             mViewStub.inflate();
 //        mViewStub.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.bt_spring_anim)
+    void onClickSpringAnim(View v) {
+//        SpringAnimation animX = new SpringAnimation(mIvSpringAnim, DynamicAnimation.TRANSLATION_X);
+//        SpringAnimation animY = new SpringAnimation(mIvSpringAnim, DynamicAnimation.TRANSLATION_Y);
+        SpringAnimation animationX = new SpringAnimation(mIvSpringAnim, SpringAnimation.SCALE_X, 1f);
+        SpringAnimation animationY = new SpringAnimation(mIvSpringAnim, SpringAnimation.SCALE_Y, 1f);
+        animationX.getSpring().setStiffness(SpringForce.STIFFNESS_LOW);
+        animationX.getSpring().setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+        animationX.setStartValue(0.8f);
+
+        animationY.getSpring().setStiffness(SpringForce.STIFFNESS_LOW);
+        animationY.getSpring().setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+        animationY.setStartValue(0.8f);
+
+        animationX.start();
+        animationY.start();
+
     }
 }
